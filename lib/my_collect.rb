ecfa6c2103f22b37e211c32eb4778072763b42a8
Sara Ginsburg <sara.ginsburg.ny@gmail.com>
2018-03-08 16:51:23 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,11 +1,12 @@
 def my_collect(collection)
   new_arr = []
   i = 0
-  
+
   while i < collection.length
     new_arr << yield(collection[i])
     i += 1
   end
+  new_arr
 end
 
 # my_collect do |el|
